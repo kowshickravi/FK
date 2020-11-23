dc7dd654ec90e711f2a6b5ffadf41efcd6c1d659
kowshickravi <65729225+kowshickravi@users.noreply.github.com>
2020-11-23 15:59:34 +0530

    Update ServiceCFAction.sql

--- TableMigration2/ServiceCFAction.sql
+++ TableMigration2/ServiceCFAction.sql
@@ -1,19 +1,19 @@
 CREATE TABLE IF NOT EXISTS dbo."ServiceCFAction"
 (	
-	"ActionId"				    INTEGER				NOT NULL,
-	"RuleId"				    INTEGER				NOT NULL,
-	"ActionType"		        VARCHAR(10)		    NOT NULL,
-	"DateCreated"			    TIMESTAMP		    NOT NULL,
-	"DateAmended"			    TIMESTAMP		    NOT NULL,
-	"DateDeleted"		        TIMESTAMP		    NOT NULL,
-	"WhoAmended_nt_username"	VARCHAR(255)	    NOT NULL,
-	"WhoAmended_hostname"		VARCHAR(255)	    NOT NULL,
-	"AuditId"					UUID	            NULL,
-	"AuditActionType"			CHAR(2)			    NOT NULL,
-	"AuditSequenceNo"			INTEGER				NOT NULL GENERATED ALWAYS AS IDENTITY,
-	"LoginName"				    VARCHAR(256)		NULL,
-	"HostName"				    VARCHAR(256)		NULL,
-	"ActionTime"				TIMESTAMP			NULL,
+	"ActionId"				INTEGER	                NOT NULL,
+	"RuleId"				INTEGER	                NOT NULL,
+	"ActionType"		                VARCHAR(10)		NOT NULL,
+	"DateCreated"			        TIMESTAMP		NOT NULL,
+	"DateAmended"			        TIMESTAMP		NOT NULL,
+	"DateDeleted"		                TIMESTAMP		NOT NULL,
+	"WhoAmended_nt_username"	        VARCHAR(255)	        NOT NULL,
+	"WhoAmended_hostname"		        VARCHAR(255)	        NOT NULL,
+	"AuditId"				UUID	                NULL,
+	"AuditActionType"			CHAR(2)			NOT NULL,
+	"AuditSequenceNo"			INTEGER			NOT NULL GENERATED ALWAYS AS IDENTITY,
+	"LoginName"				VARCHAR(256)		NULL,
+	"HostName"				VARCHAR(256)		NULL,
+	"ActionTime"				TIMESTAMP		NULL,
 	CONSTRAINT "PK_ServiceCFAction" PRIMARY KEY  
 	(
 		"AuditSequenceNo"
