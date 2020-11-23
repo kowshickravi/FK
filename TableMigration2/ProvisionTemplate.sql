c601eb900aa3b4fb41ebc4a33f70f1c6f555861d
kowshickravi <65729225+kowshickravi@users.noreply.github.com>
2020-11-23 15:09:37 +0530

    Update ProvisionTemplate.sql

--- TableMigration2/ProvisionTemplate.sql
+++ TableMigration2/ProvisionTemplate.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS dbo."ProvisionTemplate"
 (
-    "CustomerId"      INTEGER                                NOT NULL,
-	"DomainId"        INTEGER                                NOT NULL,
-	"TemplateType"    CHAR(2)                                NOT NULL,
-	"Region"          VARCHAR(10)                            NOT NULL,
-	"AuditId"         UUID                                   NULL,
-	"AuditActionType" CHAR(2)                                NOT NULL,
-	"AuditSequenceNo" INTEGER GENERATED ALWAYS AS IDENTITY   NOT NULL, 
-	"LoginName"       VARCHAR(256)                           NULL,
-	"HostName"        VARCHAR(256)                           NULL,
-	"ActionTime"      TIMESTAMP                              NULL,
+        "CustomerId"          INTEGER                                NOT NULL,
+	"DomainId"            INTEGER                                NOT NULL,
+	"TemplateType"        CHAR(2)                                NOT NULL,
+	"Region"              VARCHAR(10)                            NOT NULL,
+	"AuditId"             UUID                                   NULL,
+	"AuditActionType"     CHAR(2)                                NOT NULL,
+	"AuditSequenceNo"     INTEGER GENERATED ALWAYS AS IDENTITY   NOT NULL, 
+	"LoginName"           VARCHAR(256)                           NULL,
+	"HostName"            VARCHAR(256)                           NULL,
+	"ActionTime"          TIMESTAMP                              NULL,
 
      CONSTRAINT  "PK_ProvisionTemplate_CU01" PRIMARY KEY
 	 (
