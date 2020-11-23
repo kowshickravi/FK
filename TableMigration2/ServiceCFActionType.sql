0c910ac7e8a9784f2589bbc7033cdb8bb894f7f5
kowshickravi <65729225+kowshickravi@users.noreply.github.com>
2020-11-23 16:17:15 +0530

    Update ServiceCFActionType.sql

--- TableMigration2/ServiceCFActionType.sql
+++ TableMigration2/ServiceCFActionType.sql
@@ -1,16 +1,16 @@
 CREATE TABLE IF NOT EXISTS  dbo."ServiceCFActionType"
 (	
-	"ActionType"			VARCHAR(10)		   NOT NULL,
-	"XMLSchemaElement"	    VARCHAR(255)	    NOT NULL,
-	"[Description]"		    varchar(255)		NULL,
-	"AuditId"				UUID	            NULL,
-	"AuditActionType"		CHAR(2)			    NOT NULL,
-	"AuditSequenceNo"		INTEGER			    NOT NULL GENERATED ALWAYS AS IDENTITY,
+	"ActionType"			    VARCHAR(10)		        NOT NULL,
+	"XMLSchemaElement"	            VARCHAR(255)	        NOT NULL,
+	"[Description]"		            VARCHAR(255)		NULL,
+	"AuditId"		 	    UUID	                NULL,
+	"AuditActionType"		    CHAR(2)			NOT NULL,
+	"AuditSequenceNo"		    INTEGER			NOT NULL GENERATED ALWAYS AS IDENTITY,
 	"LoginName"			    VARCHAR(256)		NULL,
 	"HostName"			    VARCHAR(256)		NULL,
-	"ActionTime"			TIMESTAMP			NULL,
-	"ActionValue"			INTEGER		        NULL,
-	"ActionCategory"		VARCHAR(50)		    NULL,
+	"ActionTime"                        TIMESTAMP			NULL,
+	"ActionValue"                       INTEGER		        NULL,
+	"ActionCategory"		    VARCHAR(50)		        NULL,
 	CONSTRAINT "PK_ServiceCFActionType" PRIMARY KEY   
 	(
 		"AuditSequenceNo"
