07834b5d133a204c4a3519e48ced581a1f5af475
kowshickravi <65729225+kowshickravi@users.noreply.github.com>
2020-11-23 15:47:47 +0530

    Update ScalarParam.sql

--- TableMigration2/ScalarParam.sql
+++ TableMigration2/ScalarParam.sql
@@ -1,19 +1,19 @@
 CREATE TABLE IF NOT EXISTS dbo."ScalarParam"
 	(
-		"ScalarParamId"				    INTEGER				    NOT NULL,
-		"ScalarParamGroupId"			INTEGER					NOT NULL,
-		"ScalarParamKeyId"			    SMALLINT				NOT	NULL,
-		"ScalarValue"					VARCHAR(1000)			NULL,
-		"DateCreated"					TIMESTAMP			    NOT NULL,
-		"DateAmended"					TIMESTAMP			    NOT NULL,
-		"WhoAmended_nt_username"		VARCHAR(255)		    NOT NULL,
-		"WhoAmended_hostname"			VARCHAR(255)		    NOT NULL,
-		"AuditId"						UUID		            NULL,
-		"AuditActionType"				CHAR(2)				    NOT NULL,
-		"AuditSequenceNo"				INTEGER					NOT NULL GENERATED ALWAYS AS IDENTITY,
+		"ScalarParamId"				            INTEGER				NOT NULL,
+		"ScalarParamGroupId"			            INTEGER			        NOT NULL,
+		"ScalarParamKeyId"			            SMALLINT			        NOT NULL,
+		"ScalarValue"					    VARCHAR(1000)		        NULL,
+		"DateCreated"					    TIMESTAMP			        NOT NULL,
+		"DateAmended"					    TIMESTAMP			        NOT NULL,
+		"WhoAmended_nt_username"		            VARCHAR(255)		        NOT NULL,
+		"WhoAmended_hostname"			            VARCHAR(255)		        NOT NULL,
+		"AuditId"				            UUID		                NULL,
+		"AuditActionType"				    CHAR(2)				NOT NULL,
+		"AuditSequenceNo"				    INTEGER			        NOT NULL GENERATED ALWAYS AS IDENTITY,
 		"LoginName"					    VARCHAR(256)			NULL,
 		"HostName"					    VARCHAR(256)			NULL,
-		"ActionTime"					TIMESTAMP				NULL,
+		"ActionTime"					    TIMESTAMP				NULL,
 		CONSTRAINT "PK_ScalarParam_CU0" PRIMARY KEY  
 		(
 		  "AuditSequenceNo"
