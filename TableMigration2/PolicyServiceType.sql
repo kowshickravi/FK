0d165900ad9a1c9c9b9c9d5b903d7ce357a88a2f
kowshickravi <65729225+kowshickravi@users.noreply.github.com>
2020-11-23 15:07:11 +0530

    Update PolicyServiceType.sql

--- TableMigration2/PolicyServiceType.sql
+++ TableMigration2/PolicyServiceType.sql
@@ -1,18 +1,18 @@
 CREATE TABLE IF NOT EXISTS dbo."PolicyServiceType"
 	(
 		"PolicyServiceTypeId"	        SMALLINT			NOT NULL,
-		"ParentTypeId"				    INTEGER				NULL,
-		"Description"				    VARCHAR(255)		NULL,
-		"DateCreated"				    TIMESTAMP			NOT NULL,
-		"DateAmended"					TIMESTAMP			NOT NULL,
-		"WhoAmended_nt_username"		VARCHAR(255)		NOT NULL,
-		"WhoAmended_hostname"			VARCHAR(255)		NOT NULL,
-		"AuditId"						UUID		        NULL,
-		"AuditActionType"				CHAR(2)				NOT NULL,
-		"AuditSequenceNo"				INTEGER				NOT NULL GENERATED ALWAYS AS IDENTITY,
-		"LoginName"					    VARCHAR(256)		NULL,
-		"HostName"					    VARCHAR(256)		NULL,
-		"ActionTime"					TIMESTAMP			NULL,
+		"ParentTypeId"		        INTEGER				NULL,
+		"Description"		        VARCHAR(255)		        NULL,
+		"DateCreated"		        TIMESTAMP			NOT NULL,
+		"DateAmended"			TIMESTAMP			NOT NULL,
+		"WhoAmended_nt_username"	VARCHAR(255)		        NOT NULL,
+		"WhoAmended_hostname"	        VARCHAR(255)		        NOT NULL,
+		"AuditId"		        UUID		                NULL,
+		"AuditActionType"	        CHAR(2)				NOT NULL,
+		"AuditSequenceNo"		INTEGER				NOT NULL GENERATED ALWAYS AS IDENTITY,
+		"LoginName"			VARCHAR(256)		        NULL,
+		"HostName"			VARCHAR(256)		        NULL,
+		"ActionTime"		        TIMESTAMP			NULL,
 		CONSTRAINT "PK_PolicyServiceType_CU01" PRIMARY KEY  
 		(
 			"AuditSequenceNo"
