c31dc9a431b41c4203f7eed40c7ccd60552d5763
kowshickravi <65729225+kowshickravi@users.noreply.github.com>
2020-11-23 15:55:31 +0530

    Update ServerType.sql

--- TableMigration2/ServerType.sql
+++ TableMigration2/ServerType.sql
@@ -1,13 +1,13 @@
 CREATE TABLE  IF NOT EXISTS dbo."ServerType"
 (	
-	"ServerTypeId"	INTEGER				NOT NULL,
-	"[Description]"	VARCHAR(255)	    NOT NULL,
-	"AuditId"		UUID	            NULL,
-	"AuditActionType" CHAR(2)			NOT NULL,
-	"AuditSequenceNo" INTEGER			NOT NULL GENERATED ALWAYS AS IDENTITY,
+	"ServerTypeId"	          INTEGER               NOT NULL,
+	"[Description]"	          VARCHAR(255)	        NOT NULL,
+	"AuditId"		  UUID	                NULL,
+	"AuditActionType"         CHAR(2)	        NOT NULL,
+	"AuditSequenceNo"         INTEGER		NOT NULL GENERATED ALWAYS AS IDENTITY,
 	"LoginName"		  VARCHAR(256)		NULL,
 	"HostName"		  VARCHAR(256)		NULL,
-	"ActionTime"	  TIMESTAMP			NULL,
+	"ActionTime"	          TIMESTAMP		NULL,
 	CONSTRAINT "PK_ServerType" PRIMARY KEY   
 	(
 	    "AuditSequenceNo"
