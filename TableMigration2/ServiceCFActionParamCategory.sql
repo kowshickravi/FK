c5ac5719ccae04e5d886b605bac5d667391728dc
kowshickravi <65729225+kowshickravi@users.noreply.github.com>
2020-11-23 16:03:35 +0530

    Update ServiceCFActionParamCategory.sql

--- TableMigration2/ServiceCFActionParamCategory.sql
+++ TableMigration2/ServiceCFActionParamCategory.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS  dbo."ServiceCFActionParamCategory"
 (	
-	"ParamCategory"	VARCHAR(10)		    NOT NULL,
-	"[Description]"	VARCHAR(255)		NULL,
-	"AuditId"		UUID	            NULL,
-	"AuditActionType" CHAR(2)			NOT NULL,
-	"AuditSequenceNo" INTEGER		    NOT NULL GENERATED ALWAYS AS IDENTITY,
+	"ParamCategory"	          VARCHAR(10)		NOT NULL,
+	"[Description]"	          VARCHAR(255)		NULL,
+	"AuditId"		  UUID	                NULL,
+	"AuditActionType"         CHAR(2)		NOT NULL,
+	"AuditSequenceNo"         INTEGER		NOT NULL GENERATED ALWAYS AS IDENTITY,
 	"LoginName"		  VARCHAR(256)		NULL,
 	"HostName"		  VARCHAR(256)		NULL,
-	"ActionTime"	  TIMESTAMP	        NULL,
+	"ActionTime"	          TIMESTAMP	        NULL,
 	CONSTRAINT "PK_ServiceCFActionParamCategory" PRIMARY KEY  
 	(
 		"AuditSequenceNo"
